e28a926bc5d0f9c4984f1fa984e61b494aa58bcb
Andre V <andrevezi@gmail.com>
2020-05-12 17:30:24 -0300

    Task 3 - added compare 3 numbers program

---
+++ bloco_04/aula_01/exercises/ex03_retorneMaior-2.js
@@ -0,0 +1,10 @@
+let a=1 , b=1 , c=4;
+if(a>b && a>c){
+    console.log("O maior valor é: " +a);
+}
+else if(b>a && b>c){
+    console.log("O maior valor é: "+ b);
+}
+else{ 
+  console.log("O maior valor é: "+ c);
+}
